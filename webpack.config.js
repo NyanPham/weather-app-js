cd1e7d1bb0bf6d728827b837a6456f9d0afd9006
NyanPham <phamthanhnhanussh@gmail.com>
2022-02-15 10:17:48 +0700

    m

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
             { test: /\.css?$/, use: ['style-loader', 'css-loader'] },
         ]
     },
-
+    mode: 'production',
     devServer: {
         static: {
             directory: 'src',
